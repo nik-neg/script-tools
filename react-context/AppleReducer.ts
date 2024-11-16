99ab7f8c64eb04b8d554e78d9cdbda3b22148fbd
alex <alex.genkin.13@gmail.com>
2024-11-16 11:32:16 +0200

    feature: mvp

--- react-context/AppleReducer.ts
+++ react-context/AppleReducer.ts
@@ -2,12 +2,20 @@
 import { IAppleData, } from './types';
 
 export enum AppleActions {
+    FETCH_APPLES = 'FETCH_APPLES',
+    FIND_APPLE = 'FIND_APPLE',
     REMOVE_APPLE = 'REMOVE_APPLE',
     ADD_APPLE = 'ADD_APPLE'
 }
 
 export const AppleReducer = (state: IAppleData, action: any): IAppleData => {
     switch (action.type) {
+        case AppleActions.FETCH_APPLES:
+            // Handle FETCH_APPLES action
+            return state;
+        case AppleActions.FIND_APPLE:
+            // Handle FIND_APPLE action
+            return state;
         case AppleActions.REMOVE_APPLE:
             // Handle REMOVE_APPLE action
             return state;
